92bf20a08ce0d4cf8c98fa947e954c35c1da7a1a
Suvadeep Maity <68159874+AndrewSuv@users.noreply.github.com>
2021-02-20 13:31:36 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Shreya" && b=="19")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
